bfec01855b62890f01abd171c34bde540b8b4184
202255605 <sunghan1228@naver.com>
2024-12-26 21:48:51 +0900

    feat: chapter04 assertThat 테스트 진행중

--- Chapter04/src/main/java/me/sunghan/TestService.java
+++
@@ -1,4 +0,0 @@
-package me.sunghan;
-
-public class TesrService {
-}
---
+++ chapter04/src/test/java/JUNIT.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class JUNIT {
+
+    /*
+    @DisplayName() 은 테스트의 이름을 출력해주기 위한 애너테이션이다.DisplayName
+    @Test 는 애너테이셔능 ㄹ붙이면 메서드는 테스트를 수행하기 위한 메서드가 된다
+    JUNIT은 테스트끼리 영향을 주지 않고 각 테스트를 실행할때마다 테스트를 위한 객체를 만들고
+
+    .assertEquals 메서드는 첫번쨰 인수헤는 ㄴ내가 기대하는 값
+    두번쨰 인수에는 실제로 검증할 값을 argument로 빋습니다 0> 근ㄷ 이거 안쓸겁니다
+    */
+
+
+}
